c27da3d0d6a5309aa053d87830b81f2a9b4fe161
unknown <joekim2143@gmail.com>
2023-11-30 10:02:03 -0500

    q

---
+++ fromis.js
@@ -0,0 +1,46 @@
+export const FROMIS = [
+    {
+      name: "00_jun",
+      radius: 33 / 2,
+    },
+    {
+      name: "01_ssun",
+      radius: 48 / 2,
+    },
+    {
+      name: "02_ggo",
+      radius: 61 / 2,
+    },
+    {
+      name: "03_shun",
+      radius: 69 / 2,
+    },
+    {
+      name: "04_ssun",
+      radius: 89 / 2,
+    },
+    {
+      name: "05_chang",
+      radius: 114 / 2,
+    },
+    {
+      name: "06_rom",
+      radius: 129 / 2,
+    },
+    {
+      name: "07_ha",
+      radius: 156 / 2,
+    },
+    {
+      name: "08_jun",
+      radius: 177 / 2,
+    },
+    {
+      name: "09_hun",
+      radius: 220 / 2,
+    },
+    {
+      name: "10_fromis",
+      radius: 259 / 2,
+    },
+  ];
---
+++ main.js
@@ -0,0 +1,171 @@
+import {Bodies, Body, Engine, Events, Render, Runner, World} from "matter-js";
+import { FROMIS } from "./fromis";
+
+const engine = Engine.create();
+const render = Render.create({
+  engine,
+  element: document.body,
+  options:{
+    wireframes: false,
+    background: "#F7F4C8",
+    width: 620,
+    height: 850,
+  }
+});
+
+const world = engine.world;
+
+const leftWall=Bodies.rectangle(15, 396, 30, 790, {
+  isStatic: true,
+  render:{ fillStyle: "#E6B143" }
+});
+
+const rightWall=Bodies.rectangle(605, 396, 30, 790, {
+  isStatic: true,
+  render:{ fillStyle: "#E6B143" }
+});
+
+const ground=Bodies.rectangle(310, 820, 620, 60, {
+  isStatic: true,
+  render:{ fillStyle: "#E6B143" }
+});
+const topLine = Bodies.rectangle(310, 150, 620, 2, {
+  name: "topLine",
+  isStatic: true,
+  isSensor: true,
+  render:{ fillStyle: "#E6B143" }
+});
+World.add(world, [leftWall, rightWall, ground, topLine]);
+
+Render.run(render);
+Runner.run(engine);
+
+let currentBody = null;
+let currentFromis = null;
+let disableAction = false;
+let interval = null;
+let num_fromis=0;
+
+function addFromis() {
+  const index = Math.floor(Math.random() * 5);;
+  const fromis = FROMIS[index];
+
+  const body = Bodies.circle(300, 50, fromis.radius,{
+    index: index,
+    isSleeping: true,
+    render: {
+      sprite: { texture: `${fromis.name}.png`}
+    },
+    restitution: 0.3,
+  });
+
+  currentBody = body;
+  currentFromis = fromis;
+
+  World.add(world, body);
+
+}
+
+window.onkeydown = (event)=> {
+  if (disableAction){
+    return;
+  }
+  switch (event.code){
+    case "KeyA":
+      if (interval)
+        return;
+      interval = setInterval(() => {
+        if (currentBody.position.x - currentFromis.radius > 30)
+          Body.setPosition(currentBody,{
+            x: currentBody.position.x - 1,
+            y: currentBody.position.y,
+          });
+      }, 5);
+      break;
+    
+    case "KeyD":
+      if (interval)
+        return;
+      interval = setInterval(() => {
+        if(currentBody.position.x + currentFromis.radius < 590)
+          Body.setPosition(currentBody,{
+            x: currentBody.position.x + 1,
+            y: currentBody.position.y,
+          });
+        }, 5);
+      break;
+    
+    case "KeyS":
+      currentBody.isSleeping = false;
+      disableAction = true;
+
+      setTimeout(() => {
+        addFromis();
+        disableAction = false;
+      }, 1000);
+      
+      break;
+  
+  }
+}
+
+window.onkeyup = (event)=> {
+  switch (event.code){
+    case "KeyA":
+    case "KeyD":
+      clearInterval(interval);
+      interval = null;
+  }
+}
+Events.on(engine, "collisionStart", (event) => {
+  event.pairs.forEach((collision) => {
+    if (collision.bodyA.index === collision.bodyB.index) {
+      const index = collision.bodyA.index;
+
+      if (index === FROMIS.length - 1) {
+        return;
+      }
+      World.remove(world, [collision.bodyA, collision.bodyB]);
+
+      const newFromis = FROMIS[index + 1];
+
+      const newBody = Bodies.circle(
+        collision.collision.supports[0].x,
+        collision.collision.supports[0].y,
+        newFromis.radius,
+        {
+          render: {
+            sprite: { texture: `${newFromis.name}.png` }
+          },
+          index: index + 1,
+        }
+      );
+
+      World.add(world, newBody);
+
+      if (newFromis === FROMIS[10]){
+        num_fromis++;
+      }
+      if (num_fromis === 2){
+        alert("이겼따!")
+      }
+
+
+
+      
+   
+
+    }
+
+    
+
+
+    if (
+      !disableAction &&
+      (collision.bodyA.name === "topLine" || collision.bodyB.name === "topLine")) {
+      alert("Game over");
+    }
+  });
+});
+
+addFromis();
